c8bcd7f8a4fcc6d08a3135182a72093c059963c4
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2024-11-25 21:46:48 -0500

    Added Stonetalon Ruins Enemies

---
+++ StonetalonRuins/Nessie(600662).sql
@@ -0,0 +1,12 @@
+DELETE FROM `creature_template` WHERE (`entry` = 600662);
+INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
+(600662, 0, 0, 0, 0, 0, 'Nessie', 'Delve Boss', NULL, 0, 84, 84, 0, 168, 0, 1, 1.14286, 1, 1, 20, 1, 3, 0, 35, 2000, 2000, 1, 1, 1, 33536, 2048, 0, 0, 0, 0, 0, 0, 1, 4, 0, 0, 0, 0, 0, 0, 0, '', 1, 1, 100, 1, 1, 1, 0, 0, 1, 617299803, 0, 2, '', 12340);
+
+DELETE FROM `creature_template_model` WHERE (`CreatureID` = 600662);
+INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
+(600662, 0, 10244, 1, 1, 12340);
+
+DELETE FROM `creature` WHERE (`id1` = 600662);
+INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, `CreateObject`, `Comment`) VALUES
+(9001396, 600662, 0, 0, 1, 0, 0, 1, 1, 0, 2773.29, 2301.8, 191.904, 0.0884714, 300, 15, 0, 595700, 0, 1, 0, 0, 0, '', NULL, 0, NULL);
+
