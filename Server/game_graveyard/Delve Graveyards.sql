62f6b5d385d55b6d16b0cbea6102e3a9adfc6c64
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2025-08-23 19:38:54 -0400

    Create Delve Graveyards.sql

---
+++ Server/game_graveyard/Delve Graveyards.sql
@@ -0,0 +1,11 @@
+INSERT INTO `acore_world`.`game_graveyard` (`ID`, `Map`, `x`, `y`, `z`, `Comment`) VALUES
+(5000, 900, 2723.209, 1978.284, 285.9737, 'Stonetalon Ruins GY'),
+(5001, 901, 9948.516, -387.6605, 12.00058, 'Demonyunter Cove GY'),
+(5003, 904, 2504.312, 2591.502, 63.51176, 'Blackchar Cave GY'),
+(5002, 902, 1685.99, 1774.264, 144.7897, 'Stonetalon Logging Camp GY'),
+(5007, 908, 4233.918, -2685.459, 5.032512, 'Quelthalas Island GY'),
+(5004, 905, -12978.39, -1281.253, 45.77729, 'Sandfury Oasis GY'),
+(5005, 906, -1996.719, -4041.042, 3.033362, 'Arathi Farmlands GY'),
+(5006, 907, -8357.601, -341.7997, 146.8458, 'Elwynnfalls GY'),
+(5008, 909, -5929.585, 965.1906, 486.6748, 'Forgotten Gnome Camp GY'),
+(5009, 903, 2610.055, 2829.301, 35.44543, 'Old Ironforge GY');
