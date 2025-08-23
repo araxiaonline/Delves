8c0aaf7cc2b1f2ed4c32eb18ade0e3d57ebf9aa5
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2025-08-23 19:43:56 -0400

    Update Delve Graveyard Zone.sql

--- Server/graveyard_zone/Delve Graveyard Zone.sql
+++ Server/graveyard_zone/Delve Graveyard Zone.sql
@@ -1,11 +1,11 @@
 INSERT INTO `acore_world`.`graveyard_zone` (`ID`, `GhostZone`, `Faction`, `Comment`) VALUES
 (5000, 6003, 0, 'Stonetalon Ruins GY'),
 (5001, 6004, 0, 'Demonyunter Cove GY'),
-(5003, 6006, 0, 'Blackchar Cave GY'),
 (5002, 6005, 0, 'Stonetalon Logging Camp GY'),
-(5007, 6009, 0, 'Quelthalas Island GY'),
+(5003, 6006, 0, 'Blackchar Cave GY'),
 (5004, 6007, 0, 'Sandfury Oasis GY'),
 (5005, 6008, 0, 'Arathi Farmlands GY'),
-(5006, 6010, 0, 'Elwynnfalls GY'),
+(5006, 6009, 0, 'Elwynnfalls GY'),
+(5007, 6010, 0, 'Quelthalas Island GY'),
 (5008, 6011, 0, 'Forgotten Gnome Camp GY'),
 (5009, 6012, 0, 'Old Ironforge GY');
