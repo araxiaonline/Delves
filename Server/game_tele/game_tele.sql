48314c2b679c0e8a3ec7848510bee8088fe022f6
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2025-08-24 15:11:21 -0400

    Create game_tele.sql

---
+++ Server/game_tele/game_tele.sql
@@ -0,0 +1,23 @@
+INSERT INTO `acore_world`.`game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES
+-- Stonetalon Ruins
+(4000, 2725.531738, 1947.953735, 290.990601, 1.344944, 900, 'Stonetalon Ruins'),
+-- Demon Hunter Cove
+(4001, 9648.030273, -208.490005, 21.402897, 0.441980, 901, 'Demon Hunter Cove'),
+-- Stonetalon Logging Camp
+(4002, 1700.358765, 1779.384521, 144.574692, 4.328319, 902, 'Stonetalon Logging Camp'),
+-- Blackchar Cave
+(4003, 2503.135986, 2590.451904, 62.987789, 6.176579, 904, 'Blackchar Cave'),
+-- Sandfury Delve
+(4004, -12965.552734, -1277.645020, 45.990704, 4.002834, 905, 'Sandfury Delve'),
+-- Arathi Highlands Farm
+(4005, -1913.873779, -3907.356689, 110.663895, 4.729281, 906, 'Arathi Highlands Farm'),
+-- Elwynn Falls
+(4006, -8325.285156, -340.285370, 145.863403, 6.022076, 907, 'Elwynn Falls'),
+-- Quel'Thalas Island
+(4007, 4239.342285, -2683.871338, 5.781008, 4.806246, 908, 'QuelThalas Island'),
+-- Forgotten Gnome Camp
+(4008, -5931.078613, 986.903076, 488.972839, 4.873131, 909, 'Forgotten Gnome Camp'),
+-- Newman's Landing
+(4009, -6377.274902, -847.213379, 396.180176, 6.048255, 910, 'Newman\'s Landing'),
+-- Altar of Storms
+(4010, -11627.419922, -2348.380000, 10.088909, 3.813848, 911, 'Altar of Storms');
