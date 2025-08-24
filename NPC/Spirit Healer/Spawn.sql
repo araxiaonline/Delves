3d365cd67551a8d8a36bc9793f966693098160d7
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2025-08-23 20:01:12 -0400

    Create Spawn.sql

---
+++ NPC/Spirit Healer/Spawn.sql
@@ -0,0 +1,16 @@
+INSERT INTO `creature` 
+(`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, 
+`equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, 
+`spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, 
+`MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, 
+`CreateObject`, `Comment`) VALUES
+(900100, 6491, 0, 0, 900, 6003, 6003, 1, 1, 0, 2723.209, 1978.284, 285.9737, 0, 300, 0, 0, 1, 0, 0, 0, 0, 0, '', 0, 0, 'Stonetalon Ruins GY Spirit Healer'),
+(900101, 6491, 0, 0, 901, 6004, 6004, 1, 1, 0, 9948.516, -387.6605, 12.00058, 0, 300, 0, 0, 1, 0, 0, 0, 0, 0, '', 0, 0, 'Demonyunter Cove GY Spirit Healer'),
+(900102, 6491, 0, 0, 904, 6006, 6006, 1, 1, 0, 2504.312, 2591.502, 63.51176, 0, 300, 0, 0, 1, 0, 0, 0, 0, 0, '', 0, 0, 'Blackchar Cave GY Spirit Healer'),
+(900103, 6491, 0, 0, 902, 6005, 6005, 1, 1, 0, 1685.99, 1774.264, 144.7897, 0, 300, 0, 0, 1, 0, 0, 0, 0, 0, '', 0, 0, 'Stonetalon Logging Camp GY Spirit Healer'),
+(900104, 6491, 0, 0, 908, 6010, 6010, 1, 1, 0, 4233.918, -2685.459, 5.032512, 0, 300, 0, 0, 1, 0, 0, 0, 0, 0, '', 0, 0, 'Quelthalas Island GY Spirit Healer'),
+(900105, 6491, 0, 0, 905, 6007, 6007, 1, 1, 0, -12978.39, -1281.253, 45.77729, 0, 300, 0, 0, 1, 0, 0, 0, 0, 0, '', 0, 0, 'Sandfury Oasis GY Spirit Healer'),
+(900106, 6491, 0, 0, 906, 6008, 6008, 1, 1, 0, -1996.719, -4041.042, 3.033362, 0, 300, 0, 0, 1, 0, 0, 0, 0, 0, '', 0, 0, 'Arathi Farmlands GY Spirit Healer'),
+(900107, 6491, 0, 0, 907, 6009, 6009, 1, 1, 0, -8357.601, -341.7997, 146.8458, 0, 300, 0, 0, 1, 0, 0, 0, 0, 0, '', 0, 0, 'Elwynnfalls GY Spirit Healer'),
+(900108, 6491, 0, 0, 909, 6011, 6011, 1, 1, 0, -5929.585, 965.1906, 486.6748, 0, 300, 0, 0, 1, 0, 0, 0, 0, 0, '', 0, 0, 'Forgotten Gnome Camp GY Spirit Healer'),
+(900109, 6491, 0, 0, 903, 6012, 6012, 1, 1, 0, 2610.055, 2829.301, 35.44543, 0, 300, 0, 0, 1, 0, 0, 0, 0, 0, '', 0, 0, 'Old Ironforge GY Spirit Healer');
