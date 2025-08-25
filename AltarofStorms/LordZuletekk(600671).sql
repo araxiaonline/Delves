8e541c0ee46fa1f8357374f83b46d7a20041f977
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2025-08-25 19:56:54 -0400

    Updated Spawns

--- AltarofStorms/LordZuletekk(600671).sql
+++ AltarofStorms/LordZuletekk(600671).sql
@@ -28,7 +28,7 @@ INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `Q
 
 DELETE FROM `creature` WHERE (`id1` = 600671);
 INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, `CreateObject`, `Comment`) VALUES
-(9001455, 600671, 0, 0, 309, 0, 0, 1, 1, 0, -11888.2, -2708.79, 3.40174, 1.17846, 300, 7, 0, 659440, 0, 1, 0, 0, 0, '', NULL, 0, NULL);
+(9001455, 600671, 0, 0, 911, 0, 0, 1, 1, 0, -11888.2, -2708.79, 3.40174, 1.17846, 300, 7, 0, 659440, 0, 1, 0, 0, 0, '', NULL, 0, NULL);
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 600671;
 
