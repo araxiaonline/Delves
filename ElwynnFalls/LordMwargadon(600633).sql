be5877a32f0ad92b988931483b2672175f471f81
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2025-09-18 17:54:36 -0400

    Updated Spawn

--- ElwynnFalls/LordMwargadon(600633).sql
+++ ElwynnFalls/LordMwargadon(600633).sql
@@ -34,7 +34,7 @@ INSERT INTO `pickpocketing_loot_template` (`Entry`, `Item`, `Reference`, `Chance
 
 DELETE FROM `creature` WHERE (`id1` = 600633);
 INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, `CreateObject`, `Comment`) VALUES
-(9001195, 600633, 0, 0, 0, 0, 0, 1, 1, 1, -8635.07, -542.1, 145.242, 1.28288, 300, 0, 0, 846260, 0, 0, 0, 0, 0, '', NULL, 0, NULL);
+(9001195, 600633, 0, 0, 907, 0, 0, 1, 1, 1, -8635.07, -542.1, 145.242, 1.28288, 300, 0, 0, 846260, 0, 0, 0, 0, 0, '', NULL, 0, NULL);
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 600633;
 
