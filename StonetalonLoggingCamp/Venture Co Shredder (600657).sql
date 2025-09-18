2c79a353f95c628ffe98330ae50a4e9f8cadd905
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2025-09-18 19:22:35 -0400

    Update Spawn

--- StonetalonLoggingCamp/Venture Co Shredder (600657).sql
+++ StonetalonLoggingCamp/Venture Co Shredder (600657).sql
@@ -2,7 +2,6 @@ DELETE FROM `creature_template` WHERE (`entry` = 600657);
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
 (600657, 0, 0, 0, 0, 0, 'Venture Co. Shredder', 'Delve', NULL, 0, 83, 83, 0, 168, 0, 1.6, 1.19048, 1, 1, 20, 0.84, 1, 0, 36.9, 2000, 2000, 1, 1, 1, 32768, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 600657, 0, 0, 0, 0, 180, 240, 'SmartAI', 1, 1, 38, 1, 1, 1, 0, 51, 1, 0, 0, 0, '', 12340);
 
-
 DELETE FROM `creature_template_model` WHERE (`CreatureID` = 600657);
 INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
 (600657, 0, 1269, 1, 1, 12340);
