5deb01021d1b528721864592f1d580a914422345
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2025-11-22 17:00:05 -0500

    Update graveyard_zone.sql

--- Server/graveyard_zone.sql
+++ Server/graveyard_zone.sql
@@ -1,13 +1,13 @@
-INSERT INTO `acore_world`.`graveyard_zone` (`ID`, `GhostZone`, `Faction`, `Comment`) VALUES
-(5000, 6003, 0, 'Stonetalon Ruins GY'),
-(5001, 6004, 0, 'Demonhunter Cove GY'),
-(5002, 6005, 0, 'Stonetalon Logging Camp GY'),
-(5003, 6006, 0, 'Blackchar Cave GY'),
-(5004, 6007, 0, 'Sandfury Oasis GY'),
-(5005, 6008, 0, 'Arathi Farmlands GY'),
-(5006, 6009, 0, 'Elwynnfalls GY'),
-(5007, 6010, 0, 'Quelthalas Island GY'),
-(5008, 6011, 0, 'Forgotten Gnome Camp GY'),
-(5009, 6012, 0, 'Old Ironforge GY'),
-(5010, 6013, 0, 'Newmans Landing GY'),
-(5009, 6014, 0, 'Altar of Storms GY');
+INSERT INTO acore_world.graveyard_zone (ID,GhostZone,Faction,Comment) VALUES
+	 (5000,6003,0,'Stonetalon Ruins GY'),
+	 (5001,6004,0,'Demonhunter Cove GY'),
+	 (5002,6005,0,'Stonetalon Logging Camp GY'),
+	 (5003,6006,0,'Blackchar Cave GY'),
+	 (5004,6007,0,'Sandfury Oasis GY'),
+	 (5005,6009,0,'Arathi Farmlands GY'),
+	 (5006,6009,0,'Elwynnfalls GY'),
+	 (5007,6010,0,'Quelthalas Island GY'),
+	 (5008,6011,0,'Forgotten Gnome Camp GY'),
+	 (5009,6012,0,'Old Ironforge GY'),
+	 (5010,6013,0,'Newmans Landing GY'),
+	 (5011,6014,0,'Altar of Storms GY');
