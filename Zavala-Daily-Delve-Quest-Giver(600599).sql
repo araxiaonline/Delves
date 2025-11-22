9e0506043e9b3e76bb9679581db4e4e67930425e
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2025-11-22 17:06:40 -0500

    Update Zavala-Daily-Delve-Quest-Giver(600599).sql

--- Zavala-Daily-Delve-Quest-Giver(600599).sql
+++ Zavala-Daily-Delve-Quest-Giver(600599).sql
@@ -1,8 +1,1 @@
-DELETE FROM `creature_template` WHERE (`entry` = 600599);
-INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
-(600599, 0, 0, 0, 0, 0, 'Zavala', 'Daily Delve Quest', '', 0, 80, 80, 0, 35, 3, 1, 1.14286, 1, 1, 1, 1, 2, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 2, '', 0);
-
-DELETE FROM `creature_template_model` WHERE (`CreatureID` = 600599);
-INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
-(600599, 0, 7537, 1, 0, 0);
 
