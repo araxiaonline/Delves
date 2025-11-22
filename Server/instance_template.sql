f3ba11570dba4cf44a4bc3c29e5414948ad55339
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2025-11-22 17:01:12 -0500

    Update instance_template.sql

--- Server/instance_template.sql
+++ Server/instance_template.sql
@@ -1,12 +1,12 @@
-INSERT INTO `acore_world`.`instance_template` (`map`, `parent`, `script`, `allowMount`) VALUES
-(900, 0, '', 0),  -- Stonetalon Ruins
-(901, 0, '', 0),  -- Demon Hunter Cove
-(902, 0, '', 0),  -- Stonetalon Logging Camp
-(904, 0, '', 0),  -- Blackchar Cave
-(905, 0, '', 0),  -- Sandfury Delve
-(906, 0, '', 0),  -- Arathi Highlands Farm
-(907, 0, '', 0),  -- Elwynn Falls
-(908, 0, '', 0),  -- Quel'Thalas Island
-(909, 0, '', 0),  -- Forgotten Gnome Camp
-(910, 0, '', 0),  -- Newman's Landing
-(911, 0, '', 0);  -- Altar of Storms
+INSERT INTO acore_world.instance_template (`map`,parent,script,allowMount) VALUES
+	 (900,0,'',0),
+	 (901,0,'',0),
+	 (902,0,'',0),
+	 (904,0,'',0),
+	 (905,0,'',0),
+	 (906,0,'',0),
+	 (907,0,'',0),
+	 (908,0,'',0),
+	 (909,0,'',0),
+	 (910,0,'',0),
+	 (911,0,'',0);
