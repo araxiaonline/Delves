d5b5cb88e7e7a9c1757e68356c65d0a255fb6688
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2025-12-05 17:08:09 -0500

    Create reference_loot_table.sql

---
+++ Server/reference_loot_table.sql
@@ -0,0 +1,12 @@
+INSERT INTO acore_world.reference_loot_template (Entry,Item,Chance,GroupId,MinCount,MaxCount,Comment) VALUES
+	 (100500,43102,35.0,2,1,3,'Frozen Orb'),
+	 (100500,47241,25.0,2,2,4,'Emblem of Triumph'),
+	 (100500,49426,18.0,2,1,2,'Emblem of Frost'),
+	 (100500,911000,12.0,1,1,1,'Ancient Dice'),
+	 (100500,910001,8.0,1,1,1,'Araxia Token'),
+	 (100500,44990,80.0,0,5,12,'Champion''s Seal'),
+	 (100500,29434,70.0,0,6,15,'Badge of Justice'),
+	 (100500,40752,60.0,0,4,10,'Emblem of Heroism'),
+	 (100500,40753,55.0,0,4,10,'Emblem of Valor'),
+	 (100500,45624,50.0,0,3,8,'Emblem of Conquest'),
+	 (100500,41596,45.0,0,2,6,'Dalaran Jewelcrafter''s Token');
